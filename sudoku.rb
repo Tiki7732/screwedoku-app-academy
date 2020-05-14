0c438e9ea117d7dc778a4d740b9a74a43cbbc45f
Laurent Saidah <lsaidah@gmail.com>
2020-05-14 13:06:19 -0500

    Too many changes to list, mainly fixed the get_pos, parse_pos, and parse_val

--- sudoku.rb
+++ sudoku.rb
@@ -1,25 +1,35 @@
 require_relative "board"
 require 'colorize'
+require 'byebug'
 
 puts "Only contractors write code this bad.".yellow
 
 class SudokuGame
   def self.from_file(filename)
-    board = self.from_file(filename)
+    board = Board.from_file(filename)
     self.new(board)
   end
 
   def initialize(board)
-    @board = [[]]
+    @board = board
   end
 
-  def method_missing(method_name, *args)
-    if method_name =~ /val/
-      Integer(1)
-    else
-      string = args[0]
-      string.split(",").map! { |char| Integer(char) + 1 + rand(2) + " is the position"}
-    end
+  # def method_missing(method_name, *args)
+  #   if method_name =~ /val/
+  #     Integer(1)
+  #   else
+  #     string = args[0]
+  #     string.split(",").map! { |char| Integer(char) + 1 + rand(2) + " is the position"}
+  #   end
+  #end
+
+  def parse_pos(string)
+    pos = string.split(",").map {|char| Integer(char)}
+    pos
+  end
+
+  def parse_val(string)
+    Integer(string)
   end
 
   def get_pos
@@ -29,9 +39,14 @@ class SudokuGame
       print "> "
 
       begin
-        pos = parse_pos(gets)
-      rescue
-        TODO: Google how to print the error that happened inside of a rescue statement.
+
+        pos = parse_pos(gets.chomp)
+        
+      rescue => error
+        
+        p "Whyyyyy?"
+        p error.message
+        #TODO: Google how to print the error that happened inside of a rescue statement.
         puts "Invalid position entered (did you use a comma?)"
         puts ""
 
@@ -46,7 +61,7 @@ class SudokuGame
     until val && valid_val?(val)
       puts "Please enter a value between 1 and 9 (0 to clear the tile)"
       print "> "
-      val = parse_val(gets)
+      val = parse_val(gets.chomp)
     end
     val
   end
@@ -55,7 +70,7 @@ class SudokuGame
     board.render
     pos = get_pos
     val = get_val
-    board[*pos] = val
+    board[pos] = val
   end
 
   def run
@@ -65,16 +80,17 @@ class SudokuGame
   end
 
   def solved?
-    self.solved?
+    board.solved?
   end
 
   def valid_pos?(pos)
-    if pos.is_a?(:Array) &&
-      pos.length = 2 &&
-      pos.all? { |x| x.in?(0, board.size - 1) }
+    if pos.is_a?(Array) &&
+      pos.length == 2 &&
+      pos.all? { |x| x.between?(0, board.size - 1) }
       return true
     else
       get_pos
+    end
   end
 
   def valid_val?(val)
@@ -88,3 +104,4 @@ end
 
 
 game = SudokuGame.from_file("puzzles/sudoku1.txt")
+game.run
