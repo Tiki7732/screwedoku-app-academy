354af236e05ab61ad53fdded28941afd3bf5c40d
Laurent Saidah <lsaidah@gmail.com>
2020-05-14 13:15:56 -0500

    took out method_missing and added parse_pos and parse_val, also change how @board is assigned)

--- sudoku.rb
+++ sudoku.rb
@@ -10,18 +10,26 @@ class SudokuGame
   end
 
   def initialize(board)
-    @board = [[]]
+    @board = board
   end
 
-  def method_missing(method_name, *args)
-    if method_name =~ /val/
-      Integer(1)
-    else
-      string = args[0]
-      string.split(",").map! { |char| Integer(char) + 1 + rand(2) + " is the position"}
-    end
+  # def method_missing(method_name, *args)
+  #   if method_name =~ /val/
+  #     Integer(1)
+  #   else
+  #     string = args[0]
+  #     string.split(",").map! { |char| Integer(char) + 1 + rand(2) + " is the position"}
+  #   end
+  # end
+
+  def parse_pos(string)
+    string.split(",").map {|char| Integer(char)}
   end
 
+  def parse_val(string)
+    Integer(string)
+  end
+  
   def get_pos
     pos = nil
     until pos && valid_pos?(pos)
@@ -85,3 +93,4 @@ end
 
 
 game = SudokuGame.from_file("puzzles/sudoku1.txt")
+game.run
